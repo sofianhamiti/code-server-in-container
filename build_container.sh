6f497eae1c87bb5857c5aec9bd1a77a3c0fbea83
Sofian Hamiti <sofian.hamiti@gmail.com>
2022-12-10 15:35:20 +0000

    adjust volume mounting and launch command

--- build_container.sh
+++ build_container.sh
@@ -1,10 +1,9 @@
 # BUILD THE CONTAINER IMAGE LOCALLY
 docker build -t ide-in-container:latest .
 
-
 # RUN CONTAINER WITH VS CODE IN IT
-# RUN THIS COMMAND IF YOU WANT TO MOUNT THE NOTEBOOK INSTANCE HOME FOLDER
-docker run --rm -it -p 8080:8080 -v /home:/home ide-in-container:latest
+# RUN THIS COMMAND IF YOU WANT TO MOUNT THE NOTEBOOK INSTANCE HOME/SAGEMAKER FOLDER
+docker run --rm -it -p 8080:8080 -v /home/ec2-user/SageMaker/:/home/SageMaker ide-in-container:latest code-server --auth none --disable-telemetry --bind-addr 0.0.0.0:8080
 
 # RUN THIS ONE OTHERWISE
-docker run --rm -it -p 8080:8080 ide-in-container:latest
+docker run --rm -it -p 8080:8080 ide-in-container:latest code-server --auth none --disable-telemetry --bind-addr 0.0.0.0:8080
